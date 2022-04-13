adf4938d0f8dce0dadf31284eb8a91c9a86854fc
EC2 Default User <ec2-user@ip-172-31-12-137.ap-northeast-1.compute.internal>
2022-04-13 12:44:09 +0900

    top画面のアニメーション完成

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
 /* global $*/
 $(function(){
-    const previewImage = (obj) => {
-        var fileReader = new FileReader();
-        fileReader.onload = () => {
-        	$('#preview').prop('src', fileReader.result);
-        };
-        fileReader.readAsDataURL(obj.files[0]);
-    };
+  // 最初のtop画面のテキストアニメーション
+  const title = $('#title').text();
+  $('#title').text('');
+  let count = 1;
+  // text_animation関数を定義
+  const text_animation = () => {
+      const word = title.substr(0, count);
+      // 挙動確認
+      console.log(word);
+      $('#title').text(word);
+      count++;
+      if(count > title.length) {
+          clearInterval(timer);
+      }
+  };
+  
+  //タイマー処理、text_animation関数の実行(0,1秒間隔でタイピングのように文字を表示)。
+  const timer = setInterval(text_animation, 100);
+  
+  // 画像アニメーション(fadein/fadeout)）
+  const images = [
+      'images/top_1.jpg', 
+      'images/top_2.jpg', 
+      'images/top_3.jpg',
+      'images/top_4.jpg',
+      'images/top_5.jpg',
+      'images/top_6.jpg',
+      'images/top_7.jpg'
+    ];
     
-    // function previewImage(obj)
-    // {
-    // 	var fileReader = new FileReader();
-    // 	fileReader.onload = (function() {
-    // 		document.getElementById('preview').src = fileReader.result;
-    // 	});
-    // 	fileReader.readAsDataURL(obj.files[0]);
-    // }
+  let ff_count = 1;
+  // opacityを使用したアニメーション。
+  const fadein_fadeout = () => {
+      $('#mainVisual img').animate({'opacity': 0}, 1000, () => {
+          $('#mainVisual img').prop('src', images[ff_count]);
+          $('#mainVisual img').animate({'opacity': 1});
+          ff_count++;
+          if(ff_count === images.length){
+              ff_count = 0;
+          }
+      });
+  };
+  
+  // 5秒間隔で実行
+  setInterval(fadein_fadeout, 5000);
+  
 });
--- views/index_view.php
+++ views/index_view.php
@@ -19,21 +19,19 @@
         <script src="js/script.js"></script>
     </head>
     <body style="background: url(images/gray.jpg);">
-        <div class="container p-2">
-            <div class="row">
-                <h1 class="col-sm-12 text-center text-primary mt-3">Facebook Clone</h1>
+        <div class="container p-1">
+            <div class="row mt-2">
+                <h1 id="title" class="col-sm-12 text-center text-primary mt-3">Let's enjoy facebook clone!</h1>
             </div>
             <?php include_once 'views/_flush_view.php'; ?>
-            <div class="row mt-4">
-                <div class="offset-sm-2 col-sm-8">
-                    <img src="images/facebook.jpg" alt="top画面" class="mainVisual">
-                </div>
+            <div id="mainVisual">
+                <img src="images/top_1.jpg" alt="最初に表示されるtop画面" class="mainVisual">
             </div>
             <div class="row mt-5">
-                <a href="signup.php" class="offset-sm-3 col-sm-6 btn btn-primary">会員登録</a>
+                <a href="signup.php" class="offset-sm-5 col-sm-2 btn btn-primary">会員登録</a>
             </div>
             <div class="row mt-3">
-                <a href="login.php" class="offset-sm-3 col-sm-6 mt-3 btn btn-danger">ログイン</a>
+                <a href="login.php" class="offset-sm-5 col-sm-2 mt-3 btn btn-danger">ログイン</a>
             </div>
         </div>
     </body>
